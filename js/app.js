9d0ce428a8e9aa4157afa8abe8d1839cb6a67778
Marlene Salinas <marlenesdr@hotmail.com>
2019-12-02 21:21:01 -0400

    version 3

--- js/app.js
+++ js/app.js
@@ -11,8 +11,6 @@ $(document).ready(function() {
     });
 
     function llenarTablero(){
-      var imagen= new Image();
-
       $("div[class^='col']").each(function(){
         for (var i=0; i<7; i++){
           num=Math.floor(Math.random()*4+1);
@@ -23,9 +21,13 @@ $(document).ready(function() {
 
     function cargar_matriz(){
       for (var x=0; x<49; x++){
-         matriz.push('n');
+        matriz.push('n');
+        $(".elemento").each(function(x,valor){
+          $(this).attr("id","n");
+        });
       }
     }
+
 
     function cargar_matriz1(){
      $(".elemento").each(function (){
@@ -34,7 +36,8 @@ $(document).ready(function() {
     }
 
     function elegir_iguales(){
-      for (var i=1; i<49; i++){
+
+      /*for (var i=1; i<49; i++){
         if ((matriz1[i]==matriz1[i+7])&&(matriz1[i+7]==matriz1[i+14])){
             matriz[i]='c';
             matriz[i+7]='c';
@@ -45,8 +48,33 @@ $(document).ready(function() {
             matriz[i+1]='c';
             matriz[i+2]='c';
         }
+      }*/
+      var $elemento=$(".elemento");
+      $(".elemento").each(function(x,valor){
+        if ((($elemento.eq(x).attr("src"))==($elemento.eq(x+1).attr("src")))&&(($elemento.eq(x+1).attr("src"))==($elemento.eq(x+2).attr("src")))){
+          $(this).eq(x).attr("id","c")
+          $(this).eq(x+1).attr("id","c")
+          $(this).eq(x+2).attr("id","c")
+        }
+      });
+    }
+
+
+    function mostrar_iguales(){
+      for (var i=0; i<49; i++){
+        if (matriz[i]=="c"){
+          $('.elemento').each(function (i, value) {
+            $(".elemento").animate({opacity:'0.5'},"slow");
+            $(".elemento").animate({opacity:'0.8'},"slow");
+            $(".elemento").animate({opacity:'0.5'},"slow");
+            $(".elemento").animate({opacity:'0.8'},"slow");
+            $(".elemento").animate({opacity:'0.5'},"slow");
+            $(".elemento").animate({opacity:'0.8'},"slow");
+          });
+        }
       }
     }
+
 
     $(function(){
       $('.btn-reinicio').click(function(){
@@ -54,6 +82,7 @@ $(document).ready(function() {
         cargar_matriz();
         cargar_matriz1();
         elegir_iguales();
+        mostrar_iguales();
       });
     });
  });
